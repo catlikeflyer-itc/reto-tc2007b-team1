26c9733e46ec238f543c8d94a4036ee8ff5d2d57
Do Hyun Nam <dollis2002@gmail.com>
2022-09-27 13:32:23 -0500

    Added more data

--- client/components/sections/signup/signupSection.jsx
+++ client/components/sections/signup/signupSection.jsx
@@ -1,38 +1,34 @@
 import React from "react";
 import InputBox from "../../inputs/inputbox";
 import Signup from "../../buttons/signup/signup";
+import datas from "../../../data/staticData.json";
 
 export default function SignupSection() {
+  const [data, setData] = React.useState({});
+  const [error, setError] = React.useState(false);
+
+  const InputDiv = ({ ref, type, id }) => (
+    <div className="grid grid-cols-3 w-full m-4">
+      <label htmlFor={id} className="col-span-1">
+        {id}
+      </label>
+      <InputBox
+        type={type}
+        id={id}
+        //onChange={() => setData({ ...data, data[ref]: e.target.value })}
+      />
+    </div>
+  );
+
   return (
     <div className="container flex flex-col m-2 p-4 justify-center items-center">
       <h1 className="text-4xl text-center font-bold mb-4">
         Registro de administrador
       </h1>
       <div className="flex flex-col justify-center items-center m-4">
-        <div className="grid grid-cols-3 w-full m-4">
-          <label htmlFor="username" className="col-span-1">
-            Usuario:{" "}
-          </label>
-          <InputBox type="text" id="username"/>
-        </div>
-        <div className="grid grid-cols-3 w-full m-4">
-          <label htmlFor="email" className="col-span-1">
-            Correo Electronico:{" "}
-          </label>
-          <InputBox type="email" id="email" />
-        </div>
-        <div className="grid grid-cols-3 w-full m-4">
-          <label htmlFor="pw" className="col-span-1">
-            Contrasena:{" "}
-          </label>
-          <InputBox type="password" id="pw"/>
-        </div>
-        <div className="grid grid-cols-3 w-full m-4">
-          <label htmlFor="cpw" className="col-span-1">
-            Confirmar contrasena:{" "}
-          </label>
-          <InputBox type="password" id="cpw" />
-        </div>
+        {datas.signup.map((data) => (
+          <InputDiv ref={data.ref} type={data.type} id={data.id} />
+        ))}
       </div>
       <Signup classX={"w-40"} />
     </div>
