7dde0ae3b374eb55cf17395665779cb8959dc985
Do Hyun Nam <dollis2002@gmail.com>
2022-10-04 08:48:18 -0500

    new

--- client-cool/components/sections/selector/selector.jsx
+++ client-cool/components/sections/selector/selector.jsx
@@ -4,7 +4,7 @@ import SelectorCard from "../../cards/selectorCard.jsx/selectorCard";
 export default function Selector({ data }) {
   return (
     <div className="container flex flex-col justify-start items-center m-4 mb-96">
-      <h1 className="text-2xl font-bold text-center text-blue-900">
+      <h1 className="text-3xl font-bold text-center text-blue-900 m-4">
         Portal de archivos
       </h1>
       <div className="grid grid-cols-2 md:grid-cols-4">
