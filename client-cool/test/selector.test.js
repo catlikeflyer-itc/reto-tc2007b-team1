094ca08c9af86bfc7fc5dfc4e027a56f56dbb4ab
Ace0x <59582360+Ace0x@users.noreply.github.com>
2022-10-09 10:54:03 -0700

    Added one routing test. Currently doesn't work

--- client-cool/test/selector.test.js
+++ client-cool/test/selector.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-import {cleanup, render, waitFor} from '@testing-library/react';
+import {cleanup, render, waitFor, fireEvent,screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import SelectorCard from '../components/cards/selectorCard.jsx/selectorCard';
 import Selector from '../components/sections/selector/selector';
@@ -10,16 +10,22 @@ import data from "../data/staticData.json";
 
 afterEach(cleanup);
 describe('Selector', () => {
+  const selectors  = data.selector.map((item, index) => (
+    <SelectorCard
+      key={index}
+      data={item}
+    />
+  ));
     it('should render the component', () => {
-        const selectors  = data.selector.map((item, index) => (
-            <SelectorCard
-              key={index}
-              data={item}
-            />
-          ));
         render(<Selector data={data.selector} />);
         for (let i = 0; i < selectors.length; i++) {
             waitFor(() => expect(selectors[i]).toBeInTheDocument());
         }
-    })
+      })
+      it('should link to /buscar', () => {
+        render(<Selector data={data.selector} />);
+        fireEvent.click(screen.getByText('Buscar'));
+        waitFor(() => expect(window.location.pathname).toContain('/buscar'));
+      })
 })
+
