43bb29edfe9b30c8cca31f397350161460eb06e6
Ace0x <59582360+Ace0x@users.noreply.github.com>
2022-10-09 18:54:01 -0700

    Removed excess code

--- client-cool/cypress/e2e/NavTests/mainAuth.cy.js
+++ client-cool/cypress/e2e/NavTests/mainAuth.cy.js
@@ -1,5 +1,3 @@
-import MainAuth from '../../../components/sections/mainAuth.jsx/mainAuth.jsx';
-import data from "../../../data/staticData.json";
 
 describe('The user should be able to register', () => {
     it('should register a new user', () => {
