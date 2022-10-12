93584e94ddc688d973964f5280db3418bf4cddbb
Do Hyun Nam <dollis2002@gmail.com>
2022-10-11 19:05:25 -0500

    Somethingworks

--- client-cool/pages/perfil/index.jsx
+++ client-cool/pages/perfil/index.jsx
@@ -9,20 +9,18 @@ export default function index() {
       <h1 className="text-3xl font-bold text-center text-blue-900 m-4">
         Mi perfil
       </h1>
-      <div className="m-4 grid grid-cols-2 rounded-lg">
-        <div className="flex flex-col justify-start items-start col-span-1 rounded-l-lg font-bold">
-          <p>Nombre:</p>
-          <p>Correo:</p>
-          <p>Puesto:</p>
-          <p>Matricula:</p>
+      {user && (
+        <div className="m-4 grid grid-cols-2 rounded-lg">
+          <div className="flex flex-col justify-start items-start col-span-1 rounded-l-lg font-bold">
+            <p>Nombre:</p>
+            <p>Correo:</p>
+          </div>
+          <div className="flex flex-col justify-start items-end col-span-1 rounded-r-lg">
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
         </div>
-        <div className="flex flex-col justify-start items-end col-span-1 rounded-r-lg">
-          <p>Nombre</p>
-          <p>Correo</p>
-          <p>Puesto</p>
-          <p>Matricula</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
