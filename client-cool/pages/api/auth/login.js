aa415634a8d0e681b809ba15bdd78697bcb6f16e
Do Hyun Nam <dollis2002@gmail.com>
2022-10-16 18:19:27 -0500

    deleted import

--- client-cool/pages/api/auth/login.js
+++ client-cool/pages/api/auth/login.js
@@ -1,5 +1,4 @@
 import clientPromise from "../../../lib/mongodb";
-import { compareSync } from "bcryptjs";
 
 export default async function handler(req, res) {
   const client = await clientPromise;
