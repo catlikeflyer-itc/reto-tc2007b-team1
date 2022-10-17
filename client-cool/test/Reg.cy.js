1744e79fab9afb9364043aafb7838cef07090f94
Ace0x <59582360+Ace0x@users.noreply.github.com>
2022-10-17 11:23:03 -0700

    Updated Log and Reg int  tests

--- client-cool/test/Reg.cy.js
+++ client-cool/test/Reg.cy.js
@@ -7,7 +7,9 @@ describe('renders Reg component properly', () => {
         cy.get('#Correo').should('exist').type('yo@gmail.com');
         cy.get('#Contraseña').should('exist').type('1111');
         cy.get('#Nombre').should('exist').type('Andrew');
-        cy.get('[id="Confirmar contraseña"]').should('exist').type('1111');
-        cy.contains('Registrar').should('exist').click();
+        cy.get('select[data-testid="select1"]').should('exist').select("Administrador").select('Administrador de área').select('Usuario');
+        cy.get('select[data-testid="select2"]').should('exist').select("Legal").select('Penal').select('Jurídica').select("Laboral").select('todo');
+        cy.get('input[id*="Expedientes permitidos"]').should('exist').type('1,2,3,4,5');
+        cy.contains('Registrar').click();
     });
 });
