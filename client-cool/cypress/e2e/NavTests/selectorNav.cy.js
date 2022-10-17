e269246ab400e7c25daff6595e5fe2796b1901cc
Ace0x <59582360+Ace0x@users.noreply.github.com>
2022-10-17 15:37:14 -0700

    Update selectorNav.cy.js

--- client-cool/cypress/e2e/NavTests/selectorNav.cy.js
+++ client-cool/cypress/e2e/NavTests/selectorNav.cy.js
@@ -1,24 +1,25 @@
 
+import './Log.cy.js';
 
 describe('Selector Nav', () => {
     it('should navigate to the correct page', () => {
-        cy.visit('http://localhost:3000/');
-        cy.get('button').contains('Registrar').click();
         cy.contains('Buscar').click();
         cy.url().should('include', '/buscar');
         cy.go('back');
-        /* Not implemented yet
         cy.contains('Subir').click();
         cy.url().should('include', '/subir');
         cy.go('back');
-        */
         cy.contains('Perfil').click();
         cy.url().should('include', '/perfil');
         cy.go('back');
-        /* Not implemented yet
         cy.contains('Historial').click();
         cy.url().should('include', '/historial');
         cy.go('back');
-        */
+        cy.contains('Generar expediente').click();
+        cy.url().should('include', '/crear-expediente');
+        cy.go('back');
+        cy.contains('Crear usuario').click();
+        cy.url().should('include', '/crear-usuario');
+        cy.go('back');
     })
 })
