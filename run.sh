56a30bb4d8d743a79df4d16c9261f45391fb0208
Do Hyun Nam <dollis2002@gmail.com>
2022-10-18 13:19:26 -0500

    run added env

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd client-cool
+echo 'MONGODB_URI="mongodb+srv://root:team1root@cluster0.tw4r2e9.mongodb.net/?retryWrites=true&w=majority"' > .env.local
 npm install
 npm run build
 npm run start
