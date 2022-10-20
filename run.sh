e3a96fa84fecae945f090d789a951bd6b70a0327
Do Hyun Nam <dollis2002@gmail.com>
2022-10-20 17:51:04 -0500

    Update sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/catlikeflyer-itc/reto-tc2007b-team1.git
-cd client-cool
+cd reto-tc2007b-team1/client-cool
 echo 'MONGODB_URI="mongodb+srv://root:team1root@cluster0.tw4r2e9.mongodb.net/?retryWrites=true&w=majority"' > .env.local
 npm install
 npm run build
